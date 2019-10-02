934cba4cb57668299a6db1bf13177aa2b57cefcc
Eric Loudon <loudon.eric@gmail.com>
2019-10-02 19:27:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,12 +2,12 @@ require_relative 'config/environment'
 require_relative 'models/piglatinizer'
 
 class App < Sinatra::Base
-  
-  get '/' do 
-  erb :user_input
-end 
 
-post '/piglatinize' do 
+  get '/' do
+  erb :user_input
+end
+
+post '/piglatinize' do
   @input = params[:user_phrase]
   @piglatin = PigLatinizer.new.piglatinize(@input)
   erb :results
